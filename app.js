d73b627e191587ecfa97e32a334dfc98ab9dc643
Zied-bousnina <zbousnina@yahoo.com>
2023-11-11 20:32:52 +0100

    d

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const formData = require('express-form-data');
 const morgan = require('morgan');
 var app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
